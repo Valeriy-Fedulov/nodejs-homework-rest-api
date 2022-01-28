400e3535d774c2d224aca02754e3c39d18bdd08d
vofm <valerafm.vofm@gmail.com>
2022-01-28 06:41:43 +0200

    Update

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,5 +1,16 @@
 import { User } from "../models/index.js";
+import Unauthorized from "http-errors";
+import jwt from "jsonwebtoken";
 
-const auth = async (req, res, next) => {};
+const { SECRET_KEY } = process.env;
+
+const auth = async (req, res, next) => {
+  const { authorization = "" } = req.headers;
+  const [bearer, token] = authorization.split(" ");
+  if (bearer !== "Bearer") {
+    throw new Unauthorized("Not authorized");
+  }
+  const { id } = jwt.verify(token, SECRET_KEY);
+};
 
 export default auth;
