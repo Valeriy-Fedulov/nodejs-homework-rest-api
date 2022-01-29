6fc4300fe8f1fe940910324e7bdcb358b23b15ba
vofm <valerafm.vofm@gmail.com>
2022-01-29 13:45:42 +0200

    Update

--- app.js
+++ app.js
@@ -1,21 +1,12 @@
 import express from "express";
 import logger from "morgan";
 import cors from "cors";
-import fs from "fs/promises";
-import moment from "moment";
 
 import "dotenv/config";
 
 import { authRouter, usersRouter, contactsRouter } from "./routes/index.js";
 
 const app = express();
-
-app.use(async (req, res, next) => {
-  const { method, url } = req;
-  const date = moment().format("DD-MM-YYYY_hh:mm:ss");
-  await fs.appendFile("server.log", `\n${method} ${url} ${date}`);
-  next();
-});
 
 const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 
--- controllers/users/getCurrent.js
+++ controllers/users/getCurrent.js
@@ -1,5 +1,3 @@
-import { User } from "../../models/index.js";
-
 const getCurrent = async (req, res, next) => {
   const { email, subscription } = req.user;
   res.status(200).json({
