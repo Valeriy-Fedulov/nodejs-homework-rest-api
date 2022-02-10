d75e2cf7455000bea4a5fc70cefa03ddf647874d
vofm <valerafm.vofm@gmail.com>
2022-02-10 07:03:24 +0200

    Update

--- controllers/auth/singup.js
+++ controllers/auth/singup.js
@@ -3,6 +3,9 @@ import Conflict from "http-errors";
 import bcrypt from "bcryptjs";
 import gravatar from "gravatar";
 import { v4 as uuidv4 } from "uuid";
+import sgMail from "@sendgrid/mail";
+
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 const singup = async (req, res, next) => {
   const { name, password, email } = req.body;
@@ -13,14 +16,24 @@ const singup = async (req, res, next) => {
   const hashPassword = bcrypt.hashSync(password, bcrypt.genSaltSync(10));
   const avatarURL = gravatar.url(email, { s: 250 });
 
-  const verificationToken = uuidv4();
-  const linkVerify = `/auth/verify/:${verificationToken}`;
-  const msg = {
-    to: email,
-    from: "valerafm.vofm@gmail.com",
-    subject: "Thank you for registration!",
-    text: `Please verify your account by clicking the link: \nhttp://${req.headers.host}${linkVerify} \n\nThank You!\n`,
-  };
+  // const verificationToken = uuidv4();
+  // const linkVerify = `/auth/verify/:${verificationToken}`;
+  // const msg = {
+  //   to: email,
+  //   from: "valerafm.vofm@gmail.com",
+  //   subject: "Thank you for registration!",
+  //   text: `Please verify your account by clicking the link: \nhttp://${req.headers.host}${linkVerify} \n\nThank You!\n`,
+  //   html: `Please verify your account by clicking the link: \nhttp://${req.headers.host}${linkVerify} \n\nThank You!\n`,
+  // };
+
+  // sgMail
+  //   .send(msg)
+  //   .then(() => {
+  //     console.log("Email sent");
+  //   })
+  //   .catch((error) => {
+  //     console.error(error);
+  //   });
 
   const result = await User.create({
     name,
